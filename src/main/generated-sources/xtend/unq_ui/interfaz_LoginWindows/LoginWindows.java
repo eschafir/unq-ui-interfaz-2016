d75c6b00ebc3858aa2b77222326f2733db205112
presaf <federicopresa@yahoo.com.ar>
2016-04-11 00:18:09 -0300

    Se agregó la clase Login

---
+++ src/main/generated-sources/xtend/unq_ui/interfaz_LoginWindows/LoginWindows.java
@@ -0,0 +1,34 @@
+package unq_ui.interfaz_LoginWindows;
+
+import java.awt.Color;
+import org.eclipse.xtext.xbase.lib.ObjectExtensions;
+import org.eclipse.xtext.xbase.lib.Procedures.Procedure1;
+import org.uqbar.arena.widgets.Label;
+import org.uqbar.arena.widgets.Panel;
+import org.uqbar.arena.windows.SimpleWindow;
+import org.uqbar.arena.windows.WindowOwner;
+import unq_ciu.gatoEncerrado.Login.Login;
+
+@SuppressWarnings("all")
+public class LoginWindows extends SimpleWindow<Login> {
+  public LoginWindows(final WindowOwner parent, final Login model) {
+    super(parent, model);
+  }
+  
+  protected void addActions(final Panel actionsPanel) {
+    throw new UnsupportedOperationException("TODO: auto-generated method stub");
+  }
+  
+  protected void createFormPanel(final Panel mainPanel) {
+    this.setTitle("Acá hay gato encerrado...");
+    Label _label = new Label(mainPanel);
+    final Procedure1<Label> _function = new Procedure1<Label>() {
+      public void apply(final Label it) {
+        it.setText("Acá hay gato encerrado...");
+        it.setFontSize(20);
+        it.setForeground(Color.GRAY);
+      }
+    };
+    ObjectExtensions.<Label>operator_doubleArrow(_label, _function);
+  }
+}
---
+++ src/main/java/unq_ui/interfaz_LoginWindows/LoginWindows.xtend
@@ -0,0 +1,30 @@
+package unq_ui.interfaz_LoginWindows
+
+import unq_ciu.gatoEncerrado.Login.Login
+import org.uqbar.arena.windows.SimpleWindow
+import org.uqbar.arena.windows.WindowOwner
+import org.uqbar.arena.widgets.Panel
+import org.uqbar.arena.widgets.Label
+import java.awt.Color
+
+class LoginWindows extends SimpleWindow<Login>{
+	
+	new(WindowOwner parent, Login model) {
+		super(parent, model)
+	}
+	
+	override protected addActions(Panel actionsPanel) {
+		throw new UnsupportedOperationException("TODO: auto-generated method stub")
+	}
+	
+	override protected createFormPanel(Panel mainPanel) {
+		this.title = "Acá hay gato encerrado..."
+		
+		new Label(mainPanel) => [
+			text = "Acá hay gato encerrado..."
+			fontSize = 20
+			foreground = Color.GRAY
+		]
+	}
+	
+}
